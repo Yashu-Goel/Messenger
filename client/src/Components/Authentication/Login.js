91ff42b88b8f0c40c310f125c8d8ffbf7f4c53b5
Yashu <yashugoeldpsi@gmail.com>
2023-03-26 17:00:03 +0530

    Login page connected to DB

--- client/src/Components/Authentication/Login.js
+++ client/src/Components/Authentication/Login.js
@@ -9,67 +9,88 @@ import {
   Button,
   Text
 } from '@chakra-ui/react'
-
+const API_BASE = "http://localhost:5000/";
 const Login = () => {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  const [password, setPassword] = useState('');
 
   async function submitHandler(e) {
     e.preventDefault();
-    //implement the backend here
+    //implement the backend here -> OK 
+    const response = await fetch(API_BASE, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+    if (response.status === 400 || !data) {
+      window.alert("Invalid Credential");
+    } else {
+      console.log(data.token);
+      localStorage.setItem("profile", data.token);
+      window.alert(" Login Successful");
+      // navigate('/');
+    }
   }
 
   return (
-    <Stack spacing='24px'>
+    <Stack spacing="24px">
       <FormControl isRequired>
-        <FormLabel fontSize='1.5rem'>Email</FormLabel>
+        <FormLabel fontSize="1.5rem">Email</FormLabel>
         <Input
-          id='field-:r1'
+          id="field-:r1"
           required
-          aria-required='true'
-          size='lg'
-          type='email'
-          fontSize='1.5rem'
-          placeholder='Enter email address'
-          padding={'0 0 0 5px'}
+          aria-required="true"
+          size="lg"
+          type="email"
+          fontSize="1.5rem"
+          placeholder="Enter email address"
+          padding={"0 0 0 5px"}
           onChange={(e) => setEmail(e.target.value)}
-          m='0 0 1.8rem 0'
+          value={email}
+          m="0 0 1.8rem 0"
         />
 
-        <FormLabel fontSize='1.5rem'>Password</FormLabel>
-        <InputGroup size='lg'>
+        <FormLabel fontSize="1.5rem">Password</FormLabel>
+        <InputGroup size="lg">
           <Input
-            id='field-:r2'
+            id="field-:r2"
             required
-            aria-required='true'
-            type={show ? 'text' : 'password'}
-            placeholder='Enter password'
-            fontSize='1.5rem'
-            onChange={(e) => setPass(e.target.value)}
-            padding={'0 0 0 5px'}
+            aria-required="true"
+            type={show ? "text" : "password"}
+            placeholder="Enter password"
+            fontSize="1.5rem"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            padding={"0 0 0 5px"}
           />
-          <InputRightElement marginRight={'0.5rem'}>
-            <Button h='1.75rem' onClick={() => setShow(!show)} >
+          <InputRightElement marginRight={"0.5rem"}>
+            <Button h="1.75rem" onClick={() => setShow(!show)}>
               {show ? "Hide" : "Show"}
             </Button>
           </InputRightElement>
         </InputGroup>
-        <Link m='1rem 0 1.8rem 0' fontSize={'1.1rem'} float={'right'}>forgot password?</Link>
+        <Link m="1rem 0 1.8rem 0" fontSize={"1.1rem"} float={"right"}>
+          forgot password?
+        </Link>
 
         <Button
-          colorScheme={'blue'}
-          width='100%'
-          height={'3rem'}
-          fontWeight={'bold'}
-          fontSize={'15px'}
-          onClick={submitHandler}>
+          colorScheme={"blue"}
+          width="100%"
+          height={"3rem"}
+          fontWeight={"bold"}
+          fontSize={"15px"}
+          onClick={submitHandler}
+        >
           Login
         </Button>
-
       </FormControl>
     </Stack>
-  )
+  );
 }
 
 export default Login
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,40 +1,68 @@
 const express = require("express");
 const User = require("../models/userSchema");
 const router = express.Router();
-const cors= require('cors')
+const cors = require("cors");
 require("../db/conn");
 router.use(express.json());
 router.use(cors());
 
-
+//Registration(Signup)
 router.get("/", (req, res) => {
   res.send("Hello World..");
 });
 
-router.post('/', async(req,res)=>
-{
-    const {name, email, password, cpassword} = req.body
-    if(!name || !email || !password || !cpassword)
-    {
-        return res.status(422).json({ error: "Pls fill all the fields" });
-    }
+router.post("/", async (req, res) => {
+  const { name, email, password, cpassword } = req.body;
+  if (!name || !email || !password || !cpassword) {
+    return res.status(422).json({ error: "Pls fill all the fields" });
+  }
 
-    try {
-        const UserExists=await User.findOne({email:email});
-        if(UserExists)
-        {
-            return res.status(422).json({error: "User already Exists"});
-        }else if(password != cpassword)
-        {
-            return res.status(422).json({error: "Password and Confirm Password must be same!"});
-        }
-        else{
-            const user= new User({name, email, password, cpassword});
-            await user.save();
-            res.status(201).json({message: "User successfully Registered!"})
-        }
-    } catch (error) {
-        console.log("Error: " + error);
+  try {
+    const UserExists = await User.findOne({ email: email });
+    if (UserExists) {
+      return res.status(422).json({ error: "User already Exists" });
+    } else if (password != cpassword) {
+      return res
+        .status(422)
+        .json({ error: "Password and Confirm Password must be same!" });
+    } else {
+      const user = new User({ name, email, password, cpassword });
+      await user.save();
+      res.status(201).json({ message: "User successfully Registered!" });
     }
-})
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+});
+
+//Login
+// router.post("/", async (req, res) => {
+//   try {
+//     const details = req.body;
+//     const { email, password } = details;
+
+//     if (!email || !password) {
+//       return res.status(400).json({ error: "Please fill all the details" });
+//     }
+//     const userLogin = await User.findOne({ email });
+//     if (userLogin)
+//     {
+//         const isMatch = await bcrypt.compare(password, userLogin.password);
+//         const token = jwt.sign(details, "HelloIamYashu.Iamaprogrammer....");
+//         res.json({
+//           token: token,
+//           message: "Login Success!!",
+//         });
+
+//         if (!isMatch) {
+//           res.json({ message: "Invalid Credential" });
+//         } else {
+//           res.json({ message: "User signin successful" });
+//         }
+//     }
+//   } catch (error) {
+//         res.send("Invalid Credentials");
+
+//   }
+// });
 module.exports = router;
