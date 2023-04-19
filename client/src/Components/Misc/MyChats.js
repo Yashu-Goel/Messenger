c0fb65118d2c0e3baf958effa924a0bcab566cb2
Yashu <yashugoeldpsi@gmail.com>
2023-04-19 21:20:58 +0530

    Done till 24 min

--- client/src/Components/Misc/MyChats.js
+++ client/src/Components/Misc/MyChats.js
@@ -6,6 +6,7 @@ import { Button } from "@chakra-ui/button"
 import { AddIcon } from "@chakra-ui/icons"
 import ChatLoading from "../ChatLoading";
 import { getSender } from '../config/getSender';
+import GroupChatModal from './GroupChatModal';
 import axios from 'axios';
 
 const API_BASE = 'http://localhost:5000';
@@ -47,77 +48,75 @@ const MyChats = () => {
   }, [])
   return (
     <Box
-      display={{ base: selectedChat ? 'none' : 'flex', md: 'flex' }}
-      flexDir='column'
-      alignItems='center'
-      p='3'
-      bg='white'
-      w={{ base: '100%', md: '31%' }}
-      borderRadius='lg'
-      borderWidth='1px'
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p="3"
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
     >
       <Box
         pb={3}
         px={3}
-        fontSize={{ auto: '28px', md: '30px' }}
-        fontFamily='Work sans'
-        display='flex'
-        w='100%'
-        justifyContent='space-between'
-        alignItems='center'
+        fontSize={{ auto: "28px", md: "30px" }}
+        fontFamily="Work sans"
+        display="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
       >
         My Chats
-        <Button
-          display='flex'
-          fontSize={{ base: '17px', md: '10px', lg: '17px' }}
-          rightIcon={<AddIcon />}
-        >
-          New Group Chat
-        </Button>
+        <GroupChatModal>
+          <Button
+            display="flex"
+            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            rightIcon={<AddIcon />}
+          >
+            New Group Chat
+          </Button>
+        </GroupChatModal>
       </Box>
 
       <Box
-        display='flex'
-        flexDir='column'
+        display="flex"
+        flexDir="column"
         p={3}
-        bg='#F8F8F8'
-        w='100%'
-        h='100%'
-        borderRadius='lg'
-        overflowY='hidden'
+        bg="#F8F8F8"
+        w="100%"
+        h="100%"
+        borderRadius="lg"
+        overflowY="hidden"
       >
-        {
-          chats ? (
-            <Box>
-              {chats.map((chat) => (
-                <Box
-                  onClick={() => setSelectedChat(chat)}
-                  cursor={"pointer"}
-                  bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                  color={selectedChat === chat ? "white" : "black"}
-                  px={3}
-                  py={2}
-                  m={2}
-                  borderRadius='lg'
-                  key={chat._id}
-                >
-                  <Text>
-                    {!chat.isGroupChat
-                      ? getSender(loggedUser, chat.users)
-                      : chat.chatName}
-                  </Text>
-                </Box>
-              ))}
-
-
-            </Box>
-          ) : (
-            <ChatLoading />
-          )
-        }
+        {chats ? (
+          <Box>
+            {chats.map((chat) => (
+              <Box
+                onClick={() => setSelectedChat(chat)}
+                cursor={"pointer"}
+                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                color={selectedChat === chat ? "white" : "black"}
+                px={3}
+                py={2}
+                m={2}
+                borderRadius="lg"
+                key={chat._id}
+              >
+                <Text>
+                  {!chat.isGroupChat
+                    ? getSender(loggedUser, chat.users)
+                    : chat.chatName}
+                </Text>
+              </Box>
+            ))}
+          </Box>
+        ) : (
+          <ChatLoading />
+        )}
       </Box>
     </Box>
-  )
+  );
 }
 
 export default MyChats
